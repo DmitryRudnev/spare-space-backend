37891676f12f362fe40d13c9a414d0fee2b24ca9
Dmitry <dmitry.rudnevvv@gmail.com>
2026-04-16 01:15:36 +0300

    refactor(wallet): remove WalletBalance entity and enhance Wallet entity with additional properties

--- src/entities/wallet.entity.ts
+++ src/entities/wallet.entity.ts
@@ -1,15 +1,38 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn, UpdateDateColumn } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn, UpdateDateColumn, CreateDateColumn } from 'typeorm';
+import { Type } from 'class-transformer';
 import { User } from './user.entity';
+import { CurrencyType } from 'src/common/enums/currency-type.enum';
 
 @Entity('wallets')
 export class Wallet {
   @PrimaryGeneratedColumn({ type: 'bigint' })
   id: number;
 
+  @Column({ name: 'user_id', type: 'bigint' })
+  userId: number;
+
+  @Type(() => User)
   @ManyToOne(() => User, { onDelete: 'CASCADE' })
   @JoinColumn({ name: 'user_id' })
   user: User;
 
+  @Column({ type: 'decimal', precision: 26, scale: 16, default: 0 })
+  balance: number;
+
+  @Column({ type: 'enum', enum: CurrencyType })
+  currency: CurrencyType;
+
+  @Type(() => Date)
+  @CreateDateColumn({ type: 'timestamptz' })
+  createdAt: Date;
+
+  @Type(() => Date)
   @UpdateDateColumn({ type: 'timestamptz' })
   updatedAt: Date;
+
+  get formattedBalance(): number {
+    return [CurrencyType.RUB, CurrencyType.USD].includes(this.currency)
+      ? Math.round(this.balance * 100) / 100
+      : Number(this.balance);
+  }
 }
