e24203a151fafa1dbaec7602846f83347bfdbf88
Dmitry <dmitry.rudnevvv@gmail.com>
2026-05-26 19:56:21 +0300

    fix(users): register admin controller in module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
+import { UsersAdminController } from './users-admin.controller';
 import { UsersService } from './services/users.service';
 import { User } from '../entities/user.entity';
 import { UserRole } from '../entities/user-role.entity';
@@ -8,7 +9,7 @@ import { RedisModule } from '../common/redis/redis.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, UserRole]), RedisModule],
-  controllers: [UsersController],
+  controllers: [UsersController, UsersAdminController],
   providers: [UsersService],
   exports: [UsersService],
 })
